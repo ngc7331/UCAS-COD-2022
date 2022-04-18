e923a1a602274e5511a1766c7f567fd257942e30
ngc7331 <ngc7331@outlook.com>
2022-04-18 12:54:53 +0800

    feat: decode mips

--- COD_decode.py
+++ COD_decode.py
@@ -1,5 +1,14 @@
+def printTable(**table):
+    title = content = ''
+    for (key, value) in table.items():
+        title += f'{key}\t'
+        content += f'{value}\t'
+    print(title)
+    print(content)
+
+
 def main():
-    inst = input('[input] Instruction Code: ')
+    inst = input('---\n[input] Instruction Code: ')
     if inst.startswith('0x'):
         inst = bin(int(inst, 16))[2:]
     elif inst.startswith('0b'):
@@ -11,7 +20,7 @@ def main():
     print(f'Instruction Code: {inst}')
     # inst is the binary code
 
-    opcode = inst[0:6]
+    opcode = inst[:6]
     if inst.startswith('000000'):
         d = {
             '100001': 'addu',
@@ -40,10 +49,9 @@ def main():
         rt = inst[11:16]
         rd = inst[16:21]
         shamt = inst[21:26]
-        func = inst[26:32]
-        print(f'Type: r')
-        print(f'opcode\trs\trt\trd\tshamt\tfunc')
-        print(f'{opcode}\t{rs}\t{rt}\t{rd}\t{shamt}\t{func}')
+        func = inst[26:]
+        print(f'Type: R')
+        printTable(opcode=opcode, rs=rs, rt=rt, rd=rd, shamt=shamt, func=func)
         print(f'Instruction: {d[func]}')
 
     elif inst.startswith('000001'):
@@ -53,20 +61,75 @@ def main():
         }
         rs = inst[6:11]
         reg = inst[11:16]
-        imm = inst[16:32]
-        print(f'Type: regimm')
-        print(f'opcode\trs\tREG\timm')
-        print(f'{opcode}\t{rs}\t{reg}\t{imm}')
+        imm = inst[16:]
+        print(f'Type: REGIMM')
+        printTable(opcode=opcode, rs=rs, REG=reg, imm=imm)
         print(f'Instruction: {d[reg]}')
 
     elif inst.startswith('00001'):
-        t = 'j'
+        d = {
+            '0': 'j',
+            '1': 'jal',
+        }
+        instr_index = inst[6:32]
+        print(f'Type: J')
+        printTable(opcode=opcode, instr_index=instr_index)
+        print(f'Instruction: {d[opcode[5]]}')
     elif inst.startswith('0001'):
-        t = 'i-branch'
+        d = {
+            '00': 'beq',
+            '01': 'bne',
+            '10': 'blez',
+            '11': 'bgtz',
+        }
+        rs = inst[6:11]
+        rt = inst[11:16]
+        imm = inst[16:]
+        print(f'Type: I-branch')
+        printTable(opcode=opcode, rs=rs, rt=rt, imm=imm)
+        print(f'Instruction: {d[opcode[4:]]}')
     elif inst.startswith('001'):
-        t = 'i-caculate'
+        d = {
+            '001': 'addiu',
+            '111': 'lui',
+            '100': 'andi',
+            '101': 'ori',
+            '110': 'xori',
+            '010': 'slti',
+            '011': 'sltiu',
+        }
+        rs = inst[6:11]
+        rt = inst[11:16]
+        imm = inst[16:]
+        print(f'Type: I-caculate')
+        printTable(opcode=opcode, rs=rs, rt=rt, imm=imm)
+        print(f'Instruction: {d[opcode[3:]]}')
     elif inst.startswith('1'):
-        t = 'i-mem'
+        d = {
+            '0000': 'lb',
+            '0001': 'lh',
+            '0011': 'lw',
+            '0100': 'lbu',
+            '0101': 'lhu',
+            '0010': 'lwl',
+            '0110': 'lwr',
+
+            '1000': 'sb',
+            '1001': 'sh',
+            '1011': 'sw',
+            '1010': 'swl',
+            '1110': 'swr',
+        }
+        td = {
+            '0': 'load',
+            '1': 'store',
+        }
+        base = inst[6:11]
+        rt = inst[11:16]
+        offset = inst[16:]
+        print(f'Type: I-memory{td[opcode[2]]}')
+        printTable(opcode=opcode, base=base, rt=rt, offset=offset)
+        print(f'Instruction: {d[opcode[2:]]}')
     else:
         raise ValueError('invalid instruction')
 
@@ -76,5 +139,8 @@ if __name__ == '__main__':
             main()
         except KeyboardInterrupt:
             exit()
-        except Exception:
+        except KeyError:
+            print('Invalid instruction')
+        except Exception as e:
+            print(e)
             pass
